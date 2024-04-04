912bd03018de630744d2db42eca6c95596724a2a
Nikolos Z <133694947+S0L0KIN@users.noreply.github.com>
2024-04-04 02:09:09 -0400

    Movement + some restructuring of stage

--- game.asm
+++ game.asm
@@ -36,11 +36,10 @@
 
 .eqv BASE_ADDRESS 0x10008000
 
-.eqv PLAYER_SPEED 2 #define here for tweaking later
-.eqv GRAVITY 2
-.eqv TURTLE_SPEED 2
+.eqv PLAYER_SPEED 1
+.eqv GRAVITY 1
+.eqv TURTLE_SPEED 1
 .eqv DRIP_SPEED   3
-.eqv DRAGON_SPEED 4
 
 # colours
 .eqv EMPTY_HEART 0x533a20 
@@ -50,6 +49,7 @@
 .eqv WHITE 0xffffff
 .eqv BLACK 0x000000 
 .eqv GOLD 0xf1e504
+.eqv ORANGE 0xff5604
 
 .eqv HAT_D1 0xd400ff #3hp
 .eqv HAT_D2 0xeaff00 #2hp
@@ -57,33 +57,32 @@
 .eqv EDGE 0x702e0e
 .eqv BACK 0xbc5a2a
 
-.eqv TURTLE_GREEN 0x12b112
+.eqv TURTLE1_GREEN 0x12b112
 .eqv TURTLE_SPIKE 0xa29994
 .eqv STALA 0x4e4444
 .eqv ACID_GREEN 0x3eff09
-.eqv DRAGON_GREEN 0x50bc2a
+.eqv TURTLE2_GREEN 0x50bc2a
 
 .data
-MAIN_POS:	.word 6, 7  	# characters position, operating on coord grid (x, y): x,y in [0, 63] (6,7)
+MAIN_POS:	.word 6, 7   	# characters position, operating on coord grid (x, y): x,y in [0, 63] (6,7)
 MAIN_HEALTH:	.word 3		# begin with 3 hp
 
-TURTLE_POS:	.word 17, 21, 1	# start moving right (use 3rd variable 1=right -1=left) (17,21,1)
-DRAGON_POS: 	.word -1, 46	# starts offscreen (-1) on y=46
-DRAGON_TIMER:	.word 0		# in ms how long between dragon appearance
+TURTLE1_POS:	.word 17, 21, 4	# start moving right (use 3rd variable 4=right -4=left) (17,21,1)
+TURTLE2_POS: 	.word 50, 47, 4	# same as above
 
-DRIP_POS:	.word -1, 0 	# track drips (SINCE ALL ARE IN A LINE, ONLY NEED 1 COORD) change to actual pos before implement
+DRIP_POS:	.word 16, 28 	# track drips (SINCE ALL ARE IN A LINE, ONLY NEED 1 COORD) change to actual pos before implement
 DRIP_TIMER:	.word 0
 
 .text
 .globl main 
 
 #TODO:		
-# - DRAW MOBS
-# - GET IT ALL MOVING (PLAYER + ROCKS)
+# - GET IT ALL MOVING (PLAYER + ENEMIES)
 #    - HARD BOUNDARIES (edges)
-#    - DRAGON DISAPPEAR AT END
 # - COLLISION BETWEEN THEM AND PLAYER
+#    - DAMAGE
 # - RESTART
+# - victory / loss screen?
 
 
 #WANT menu (+1), shooting(?), moving enemies (+2), diff damage level colours (+2). ez 5.
@@ -97,15 +96,20 @@ main:
 loop:
 	li $a0 1 #THIS AND BELOW DELETES 
 	jal draw_player #PLAYER
-	jal draw_turtle
+	jal draw_turtle1
+	jal draw_turtle2
+	jal draw_acid
 	li $a0, 0 #RESET HARD CHECK FOR THIS CLOCK CYCLE
-	#DO SAME FOR DRIPS, DRAGON
 
 	jal handle_input
+	jal grav
 	#move enemies
 	
 	jal draw_player
-	jal draw_turtle
+	jal draw_turtle1
+	jal draw_turtle2
+	jal draw_acid
+	
 	#DO SAME FOR DRIPS, TURTLE, DRAGON
 	
 	#THIS WILL WORK LIKE
@@ -118,17 +122,12 @@ loop:
 		# if 0 game over screen, else restart at top (RESET .data, call loop again)
 	#check collision on gold
 		# if collision, victory screen
-	j exit
+		
 		
 sleep:
 	li $v0, 32 #MARS instruction for sleep
 	li $a0, 40 #sleep for 40ms
 	syscall
-
-	la $t0, DRAGON_TIMER #want to add 40ms to dragon and drip timer
-	lw $t1, 0($t0)
-	addi $t1, $t1, 40
-	sw $t1, 0($t0) #store it back
 	
 	la $t0, DRIP_TIMER #same for drip
 	lw $t1, 0($t0)
@@ -138,7 +137,115 @@ sleep:
 	j loop #restart loop
 
 handle_input:
+	li $t9, 0xffff0000
+	lw $t8, 0($t9)
+	bne $t8, 1, return
+	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000 from before
+
+	beq $t2, 97, left #a was pressed
+	beq $t2, 100, right #d
+	beq $t2, 114, restart #r
+	beq $t2, 113, exit #q
+
+	jr $ra #any other input is invalid
+
+left:
+	la $t0 BASE_ADDRESS
+	la $t1, MAIN_POS #player in memory
+	lw $t2, 0($t1) #get x in t2
+	lw $t3, 4($t1) #get y in t3
+
+	#get exact pixel location
+	sll $t2, $t2, 2 # actual x = stored x * 4 
+	sll $t3, $t3, 8 # actual y = stored y * (2^8)
+	add $t1, $t0, $t2 #base + x
+	add $t1, $t1, $t3 #base+x + y
+	add $t1, $t1, -8 #want just left of player
+	
+	lw $t4, 0($t1) #CHECK ALL 5 PIXELS LEFT OF PLAYER 
+	beq $t4, EDGE, return
+	lb $t4, 256($t1)
+	beq $t4, EDGE, return
+	lb $t4, 512($t1)
+	beq $t4, EDGE, return
+	lb $t4, -256($t1)
+	beq $t4, EDGE, return
+	lb $t4, -512($t1)
+	beq $t4, EDGE, return
+	
+	la $t0, MAIN_POS #same for drip
+	lw $t1, 0($t0)
+	addi $t1, $t1, -PLAYER_SPEED
+	sw $t1, 0($t0)
+	
 	jr $ra
+
+right:
+	la $t0 BASE_ADDRESS
+	la $t1, MAIN_POS #player in memory
+	lw $t2, 0($t1) #get x in t2
+	lw $t3, 4($t1) #get y in t3
+
+	#get exact pixel location
+	sll $t2, $t2, 2 # actual x = stored x * 4 
+	sll $t3, $t3, 8 # actual y = stored y * (2^8)
+	add $t1, $t0, $t2 #base + x
+	add $t1, $t1, $t3 #base+x + y
+	add $t1, $t1, 8 #want just right of player
+	
+	lw $t4, 0($t1) #CHECK ALL 5 PIXELS RIGHT OF PLAYER
+	beq $t4, EDGE, return
+	lb $t4, 256($t1)
+	beq $t4, EDGE, return
+	lb $t4, 512($t1)
+	beq $t4, EDGE, return
+	lb $t4, -256($t1)
+	beq $t4, EDGE, return
+	lb $t4, -512($t1)
+	beq $t4, EDGE, return
+	
+	la $t0, MAIN_POS #same for drip
+	lw $t1, 0($t0)
+	addi $t1, $t1, PLAYER_SPEED
+	sw $t1, 0($t0)
+	
+	jr $ra
+
+grav:
+	la $t0 BASE_ADDRESS
+	la $t1, MAIN_POS #player in memory
+	lw $t2, 0($t1) #get x in t2
+	lw $t3, 4($t1) #get y in t3
+
+	#get exact pixel location
+	sll $t2, $t2, 2 # actual x = stored x * 4 
+	sll $t3, $t3, 8 # actual y = stored y * (2^8)
+	add $t1, $t0, $t2 #base + x
+	add $t1, $t1, $t3 #base+x + y
+	add $t1, $t1, 768 #want just right of player
+	
+	lw $t4, 0($t1) #CHECK ALL 5 PIXELS RIGHT OF PLAYER
+	beq $t4, EDGE, return
+	lb $t4, 4($t1)
+	beq $t4, EDGE, return
+	lb $t4, -4($t1)
+	beq $t4, EDGE, return
+
+	
+	la $t0, MAIN_POS #same for drip
+	lw $t1, 4($t0)
+	addi $t1, $t1, GRAVITY
+	sw $t1, 4($t0)
+	
+	jr $ra
+
+restart:
+	
+	
+	#change all variables to default
+	
+	# j main redraw everything (cause sometimes clippy stuff wants to be fixed)
+	
 
 #player drawing
 draw_player:
@@ -177,9 +284,9 @@ load_clear: #dead
 	li $t2, BACK
 	li $t3, BACK
 	j render_player
-load_green:
+load_green: #ACID ANIMATION
 	li $t2, ACID_GREEN
-	li $t3, TURTLE_GREEN
+	li $t3, TURTLE1_GREEN
 	j render_player
 render_player:
     sw $t2, 0($t1)
@@ -194,9 +301,9 @@ render_player:
     jr $ra
     
 #turtle drawing
-draw_turtle:
+draw_turtle1:
 	la $t0, BASE_ADDRESS #framebuffer
-	la $t1, TURTLE_POS #player in memory
+	la $t1, TURTLE1_POS #player in memory
 	lw $t2, 0($t1) #get x in t2
 	lw $t3, 4($t1) #get y in t3
 
@@ -206,7 +313,24 @@ draw_turtle:
 	add $t1, $t0, $t2 #base + x
 	add $t1, $t1, $t3 #base+x + y
 	
-	li $t2, TURTLE_GREEN
+	li $t2, TURTLE1_GREEN
+	li $t3, BLACK
+	beq $a0, 1, turtle_clear
+	j render_turtle
+	
+draw_turtle2:
+	la $t0, BASE_ADDRESS #framebuffer
+	la $t1, TURTLE2_POS #player in memory
+	lw $t2, 0($t1) #get x in t2
+	lw $t3, 4($t1) #get y in t3
+
+	#get exact pixel location
+	sll $t2, $t2, 2 # actual x = stored x * 4 
+	sll $t3, $t3, 8 # actual y = stored y * (2^8)
+	add $t1, $t0, $t2 #base + x
+	add $t1, $t1, $t3 #base+x + y
+	
+	li $t2, TURTLE2_GREEN
 	li $t3, BLACK
 	beq $a0, 1, turtle_clear
 	j render_turtle
@@ -215,7 +339,6 @@ turtle_clear:
 	li $t2, BACK
 	li $t3, BACK
 	j render_turtle
-
 render_turtle:
 	sw $t2, 0($t1)
 	sw $t2, 4($t1)
@@ -230,6 +353,36 @@ render_turtle:
 	
 	jr $ra
 	
+draw_acid:
+	la $t0, BASE_ADDRESS #framebuffer
+	la $t1, DRIP_POS #player in memory
+	lw $t2, 0($t1) #get x in t2
+	lw $t3, 4($t1) #get y in t3
+	
+	sll $t2, $t2, 2 # actual x = stored x * 4 
+	sll $t3, $t3, 8 # actual y = stored y * (2^8)
+	add $t1, $t0, $t2 #base + x
+	add $t1, $t1, $t3 #base+x + y
+	
+	li $t2, ACID_GREEN
+	beq $a0, 1, acid_clear
+	j render_acid
+	
+acid_clear:
+	li $t2, BACK
+	
+render_acid:
+	sw $t2, 0($t1)
+	addi $t1 $t1 72
+	sw $t2, 0($t1)
+	addi $t1 $t1 52
+	sw $t2, 0($t1)
+	addi $t1 $t1 3212
+	sw $t2, 0($t1)
+	
+	jr $ra
+	
+
 
 #### STAGE DRAWING (happens once)
 draw_stage:
@@ -307,11 +460,11 @@ draw_holes:
 	sw $t2, 0($t4)
 	add $t4, $t4, 256
 	sw $t2, 0($t4)
-	add $t4, $t4, 3272
+	add $t4, $t4, 3264
 	sw $t2, 0($t4)
 	add $t4, $t4, 256
 	sw $t2, 0($t4)
-	add $t4, $t4, 2868
+	add $t4, $t4, 2876
 	sw $t2, 0($t4)
 	add $t4, $t4, 256
 	sw $t2, 0($t4)
@@ -338,7 +491,7 @@ draw_hearts:
 	addi $t1 $t1 1
 	j draw_hearts
 
-draw_stala:
+draw_stala: #grey spikes
 	li $t2 STALA
 	li $t1 6464
 	add $t1 $t0 $t1
@@ -350,7 +503,7 @@ draw_stala:
 	sw $t2 4($t1)
 	sw $t2 260($t1)
 	
-	addi $t1 $t1 64
+	addi $t1 $t1 72
 	sw $t2 0($t1)
 	sw $t2 -4($t1)
 	sw $t2 4($t1)
@@ -359,12 +512,20 @@ draw_stala:
 	sw $t2 260($t1)
 	sw $t2 512($t1)
 	
-	addi $t1 $t1 44
+	addi $t1 $t1 52
 	sw $t2 0($t1)
 	sw $t2 -4($t1)
 	sw $t2 4($t1)
 	sw $t2 252($t1)
 	sw $t2 256($t1)
+	sw $t2 512($t1)
+	
+	addi $t1 $t1 3212
+	sw $t2 0($t1)
+	sw $t2 -4($t1)
+	sw $t2 4($t1)
+	sw $t2 256($t1)
+	sw $t2 260($t1)
 	sw $t2 512($t1)
 	
 draw_gold: #TO BE COMPLETED!! just remember victory pixel is (40 , 60)
@@ -375,7 +536,6 @@ draw_gold: #TO BE COMPLETED!! just remember victory pixel is (40 , 60)
 	sw $t2 0($t1)
 	sw $t2 256($t1)
 	sw $t2 512($t1)
-	
 	
 return:
 	jr $ra
